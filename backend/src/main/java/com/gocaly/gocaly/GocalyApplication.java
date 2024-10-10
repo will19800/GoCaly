bcf3030847c5a57b2539a32f7480c9077ed6f4b2
Will Chen <willchen198@gmail.com>
2024-10-10 09:56:52 -0400

    initial commit

---
+++ backend/src/main/java/com/gocaly/gocaly/GocalyApplication.java
@@ -0,0 +1,13 @@
+package com.gocaly.gocaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GocalyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GocalyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gocaly/gocaly/GocalyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gocaly.gocaly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GocalyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
